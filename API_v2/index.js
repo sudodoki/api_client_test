086620701d019386c088b5baeb2de23ef7dcb3ee
xlenz <xlenz7@ua.fm>
2014-06-26 15:49:05 +0300

    Revert "#3 - user/me"
    This reverts commit a41caf2e1bb1df42d65dac26ff9f9cc9475c65e0.

--- API_v2/index.js
+++ API_v2/index.js
@@ -141,14 +141,15 @@ server.get('/user', function (req, res, next) {
 })
 
 server.get('/user/:id', forAuthorized, function (req, res, next) {
-  if (req.params.id === 'me') {
-    return res.send(200, req.user);
-  }
   db.collection('users').findOne({ _id: mongojs.ObjectId(req.headers['secret-token']), is_published: "true"}, function(err, doc) {
     if (err) { return res.send(500, { error: 'Database error:' + (err.message || 'unknown error') })}
     if (!doc) {return res.send(404, 'User does not exist')}
     return res.send(200, stripOut(doc))
   })
+})
+
+server.get('/user/me', forAuthorized, setUser, function (req, res, next) {
+  return res.send(200, req.user)
 })
 
 server.post('/user/me', forAuthorized, setUser, function (req, res, next) {
