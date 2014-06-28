21af4779d5ffd13198a223d7080a5794e7cfbd07
sudodoki <smd.deluzion@gmail.com>
2014-06-28 19:32:13 +0300

    Add CORS test for /version

--- API_v2/test/API/version.test.js
+++ API_v2/test/API/version.test.js
@@ -6,11 +6,15 @@ const app = require(root + 'index')
 const pkg = require(root + 'package.json')
 
 describe('GET /version', function(){
-  xit('should have CORS hearder', function(done){
+  it('should have CORS hearder', function(done){
     request(app)
-      .get('/version')
-      .expect(200)
-
+      .options('/version')
+      .set('Origin', 'http://mysite.com')
+      .set('Allow-Headers', 'Accept, X-Requested-With, Content-Type, SECRET-TOKEN, secret')
+      .expect(204)
+      .expect('Access-Control-Allow-Credentials', 'true')
+      .expect('Access-Control-Allow-Origin', 'http://mysite.com')
+      .expect('Access-Control-Allow-Headers', 'Accept, X-Requested-With, Content-Type, SECRET-TOKEN, secret')
       .end(done)
   })
   it('should return string', function(done){
